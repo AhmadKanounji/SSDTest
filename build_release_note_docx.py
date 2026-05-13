dc76c7801b20009386c8ad1633e151bcf554cca9
Ahmad Kanounji <113057474+AhmadKanounji@users.noreply.github.com>
2026-05-13 12:06:36 +0300

    Update build_release_note_docx.py

--- build_release_note_docx.py
+++ build_release_note_docx.py
@@ -1,8 +1,11 @@
 from docx import Document
 
-doc = Document("RN_EG_CBE_Billing_Template.docx")
+doc = Document("templates/RN_EG_CBE_Billing_Template.docx")
 
-print(len(doc.tables))
+print(f"Total tables = {len(doc.tables)}")
 
 for i, table in enumerate(doc.tables):
-    print(i, table.cell(0,0).text)
+    try:
+        print(f"TABLE {i}: {table.cell(0,0).text}")
+    except:
+        print(f"TABLE {i}: unable to read")
